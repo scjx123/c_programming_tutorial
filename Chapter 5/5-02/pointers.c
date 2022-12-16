806255d86fdd4c5027e4aecb88a7eca30fdf37a1
steve <steve_chan@live.com.sg>
2022-12-16 11:27:37 +0800

    Add chapter 5

---
+++ Chapter 5/5-02/pointers.c
@@ -0,0 +1,26 @@
+/*
+Author: Steve Chan
+Email : steve_chan@live.com.sg
+*/
+
+#include <stdio.h>
+
+int strlen(char *s){
+    char *p = s; 
+    while(*p!='\0')
+    p++; 
+    return p-s;7
+}
+
+
+
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8}; 
+    void*t=arr; 
+    int i; 
+    printf("%d\n",arr[-2]);
+    printf("%d\n",strlen("fuck"));
+   
+    return 0;
+}
